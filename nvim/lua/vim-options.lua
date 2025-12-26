278caca712159a240a0f9a7e8f3790b9da8089fe
liukushka <liukushka@liukushka.homenet.telecomitalia.it>
2025-12-26 16:47:11 +0100

    updates on the config

--- nvim/lua/vim-options.lua
+++ nvim/lua/vim-options.lua
@@ -13,3 +13,13 @@ vim.g.maplocalleader = "\\"
 
 -- Enable line numbers
 vim.cmd("set number")
+
+-- remap the way to move between windows and resize them easily
+vim.keymap.set("n", "<C-h>", "<C-w>h")
+vim.keymap.set("n", "<C-j>", "<C-w>j")
+vim.keymap.set("n", "<C-k>", "<C-w>k")
+vim.keymap.set("n", "<C-l>", "<C-w>l")
+vim.keymap.set("n", "<A-l>", [[<cmd>vertical resize +5<cr>]]) -- make the window biger vertically
+vim.keymap.set("n", "<A-h>", [[<cmd>vertical resize -5<cr>]]) -- make the window smaller vertically
+vim.keymap.set("n", "<A-k>", [[<cmd>horizontal resize +2<cr>]]) -- make the window bigger horizontally by pressing shift and =
+vim.keymap.set("n", "<A-j>", [[<cmd>horizontal resize -2<cr>]]) -- make the window smaller horizontally by pressing shift and -
